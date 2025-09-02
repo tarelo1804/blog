522b6e3fb53659d20518310fd2eb769a5f3da90c
Pamela Tarelo <pamelatarelo18@gmail.com>
2025-09-02 15:08:24 -0600

    Cambios
    Cambios en las migraciones de comentarios y likes

--- blog/database/migrations/2025_09_02_015846_comentarios_migration.php
+++ blog/database/migrations/2025_09_02_015846_comentarios_migration.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('comentarios', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id');
-            $table->integer('post_id');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('post_id')->constrained('posts');
             $table->text('content');
             $table->timestamps();
         });
